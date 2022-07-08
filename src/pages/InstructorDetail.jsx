e95c6632d915248a8c743da4c16d812fe05f6621
birkan <b.561birkan@gmail.com>
2022-07-08 13:59:33 +0300

    adding error handling

--- src/pages/InstructorDetail.jsx
+++ src/pages/InstructorDetail.jsx
@@ -1,20 +1,50 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import NotFound from "./NotFound";
+import image from "../img/loading.gif";
 
 const InstructorDetail = () => {
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
   const { id } = useParams();
   const [inst, setInst] = useState([]);
   const getInstructor = () => {
     fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
-      .then((res) => res.json())
+      .then((res) => {
+        if (!res.ok) {
+          let counter = 0;
+          const sec1 = setInterval(() => {
+            counter++;
+            if (counter > 3) {
+              clearInterval(sec1);
+              setLoading(false);
+              setError(true);
+            }
+          }, 1000);
+          throw new Error("Something went wrong");
+        }
+        setLoading(false);
+        return res.json();
+      })
+
       .then((data) => setInst(data))
       .catch((err) => console.log(err));
   };
   useEffect(() => {
     getInstructor();
   }, []);
+  if (error) {
+    return <NotFound />;
+  }
+  if (loading) {
+    return (
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <img src={image} alt="loading" style={{ width: "75vw" }} />
+      </div>
+    );
+  }
   return (
     <div className="container text-center">
       <h3>{inst.name}</h3>
